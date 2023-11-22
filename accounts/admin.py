b8f9d2fb58439465dc69e2deb655003b50058b2e
moawezz <moawez.de@gmail.com>
2023-11-22 20:56:23 +0330

    create AccountAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class AccountAdmin(UserAdmin):
+    model = CustomUser
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ['username', 'email',]
