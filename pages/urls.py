2293a31e2f29613bfd24683dab26cc423f002450
moawezz <moawez.de@gmail.com>
2023-11-22 21:17:59 +0330

    add pages urls

---
+++ pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import HomePageView, AboutUsPageView, ContactUsPageView, NotFoundPageView
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about-us/', AboutUsPageView.as_view(), name='about_us'),
+    path('contact-us/', ContactUsPageView.as_view(), name='contact_us'),
+    path('not-found/', NotFoundPageView.as_view(), name='not_found'),
+]
