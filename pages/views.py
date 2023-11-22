6cb4d3cd0e4f11f0c829dc0b7b2a745f9f0dac75
moawezz <moawez.de@gmail.com>
2023-11-22 21:18:19 +0330

    add pages views

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutUsPageView(TemplateView):
+    template_name = 'pages/about_us.html'
+
+
+class ContactUsPageView(TemplateView):
+    template_name = 'pages/contact_us.html'
+
+
+class NotFoundPageView(TemplateView):
+    template_name = 'pages/404_page.html'
