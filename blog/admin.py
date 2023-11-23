742be3fc041126c96b31f0c9849bd4b63b6e9a9e
moawezz <moawez.de@gmail.com>
2023-11-23 14:19:13 +0330

    regiser admin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentsInline(admin.StackedInline):
+    model = Comment
+    fields = ['user', 'text', 'status', 'active',]
+    extra = 1
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'author', 'slug', 'active', 'status', 'created_at']
+    list_display = ['id', 'title', 'author', 'slug', 'active', 'status', 'created_at',]
     search_fields = ['title',]
     list_editable = ['status', 'active',]
     ordering = ['created_at',]
+    inlines = [CommentsInline,]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'post', 'status', 'active', 'created_at',]
+    search_fields = ['user', 'post',]
+    list_editable = ['status', 'active',]
