9a3f224b1645cb6561205f63ba3ed3e609c08311
moawezz <moawez.de@gmail.com>
2023-11-30 08:32:45 +0330

    complete generating slug for posts

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django_extensions.db.fields import AutoSlugField
 from ckeditor.fields import RichTextField
-from slugify import slugify
+from autoslug import AutoSlugField
 
 
 def custom_slugify(value):
-    return slugify(value, separator='-', allow_unicode=True)
+    return value.replace(' ', '-')
 
 class Post(models.Model):
     POST_PUBLISHED = 'pub'
@@ -19,20 +18,19 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=255)
-    slug = AutoSlugField(populate_from=['title'], unique=True, allow_unicode=True, slugify_function=custom_slugify)
     description = RichTextField()
     author = models.ForeignKey(to=get_user_model(), related_name='posts', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='blog/post_image/', blank=True)
     status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=POST_DRAFT)
     active = models.BooleanField(default=False)
+    slug = AutoSlugField(
+        populate_from='title',
+        slugify=custom_slugify,
+        )
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = custom_slugify(self.title)
-        super(Post, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.title
