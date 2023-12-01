589e1b97337758892150cd2890a2a1f3aa81408c
moawezz <moawez.de@gmail.com>
2023-12-01 11:31:30 +0330

    add blog to config urls

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
+    path('blog/', include('blog.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
